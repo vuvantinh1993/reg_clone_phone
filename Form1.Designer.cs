f44e49e17bce7da5e04d2b934b90f741771f2333
vutinh Van <vuvantinh199320181@outlook.com>
2021-02-17 12:19:33 +0700

    update

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Tool_regnick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.thietbi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pass = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.birthday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._2fa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.passMail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbApiKey = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.thietbi,
+            this.ten,
+            this.pass,
+            this.birthday,
+            this._2fa,
+            this.email,
+            this.passMail,
+            this.status});
+            this.dataGridView1.Location = new System.Drawing.Point(7, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1022, 515);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // thietbi
+            // 
+            this.thietbi.DataPropertyName = "thietbi";
+            this.thietbi.HeaderText = "Thiết bị";
+            this.thietbi.Name = "thietbi";
+            // 
+            // ten
+            // 
+            this.ten.DataPropertyName = "ten";
+            this.ten.HeaderText = "Tên";
+            this.ten.Name = "ten";
+            // 
+            // pass
+            // 
+            this.pass.DataPropertyName = "pass";
+            this.pass.HeaderText = "Pass";
+            this.pass.Name = "pass";
+            // 
+            // birthday
+            // 
+            this.birthday.DataPropertyName = "birthday";
+            this.birthday.HeaderText = "Ngày sinh";
+            this.birthday.Name = "birthday";
+            // 
+            // _2fa
+            // 
+            this._2fa.DataPropertyName = "_2fa";
+            this._2fa.HeaderText = "2Fa";
+            this._2fa.Name = "_2fa";
+            // 
+            // email
+            // 
+            this.email.DataPropertyName = "email";
+            this.email.HeaderText = "Email";
+            this.email.Name = "email";
+            // 
+            // passMail
+            // 
+            this.passMail.DataPropertyName = "passMail";
+            this.passMail.HeaderText = "PassMail";
+            this.passMail.Name = "passMail";
+            // 
+            // status
+            // 
+            this.status.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.status.DataPropertyName = "status";
+            this.status.HeaderText = "Trạng Thái";
+            this.status.Name = "status";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(1045, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reg Nick";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbApiKey
+            // 
+            this.tbApiKey.Location = new System.Drawing.Point(1045, 91);
+            this.tbApiKey.Multiline = true;
+            this.tbApiKey.Name = "tbApiKey";
+            this.tbApiKey.Size = new System.Drawing.Size(207, 229);
+            this.tbApiKey.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(1063, 346);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Tạm Dừng";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Violet;
+            this.button3.Location = new System.Drawing.Point(1063, 405);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(168, 40);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Dừng Hẳn";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 532);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tbApiKey);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn thietbi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pass;
+        private System.Windows.Forms.DataGridViewTextBoxColumn birthday;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _2fa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn passMail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn status;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbApiKey;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tool_regnick
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task t = new Task(() =>
+            {
+                Auto();
+            });
+            t.Start();
+        }
+        void Regchrome()
+        {
+            Task t = new Task(() =>
+            {
+
+
+            });
+            t.Start();
+        }
+
+
+        void Auto()
+        {
+            List<string> devices = new List<string>();
+            var listdevices = KAutoHelper.ADBHelper.GetDevices();
+
+            foreach (var deviceID in listdevices)
+            {
+                KAutoHelper.ADBHelper.Tap(deviceID, 671, 1689);
+                Sleep(20000);
+                KAutoHelper.ADBHelper.Tap(deviceID, 540, 1855);
+                KAutoHelper.ADBHelper.Tap(deviceID, 524, 1450);
+
+                // từ chối dnah bạ
+                KAutoHelper.ADBHelper.Tap(deviceID, 528, 1257);
+                KAutoHelper.ADBHelper.Tap(deviceID, 528, 1257);
+                KAutoHelper.ADBHelper.Tap(deviceID, 547, 1990);
+
+
+                // nhập tên
+                KAutoHelper.ADBHelper.InputText(deviceID, "Vux");
+
+                KAutoHelper.ADBHelper.Tap(deviceID, 733, 581);
+                KAutoHelper.ADBHelper.InputText(deviceID, "Vaan Tinhr");
+
+                KAutoHelper.ADBHelper.Tap(deviceID, 524, 828);
+
+                // ngày tháng năm sinh
+                KAutoHelper.ADBHelper.Tap(deviceID, 358, 817);
+                KAutoHelper.ADBHelper.InputText(deviceID, "17");
+                KAutoHelper.ADBHelper.Tap(deviceID, 544, 721);
+                KAutoHelper.ADBHelper.Key(deviceID, KAutoHelper.ADBKeyEvent.KEYCODE_DEL);
+                KAutoHelper.ADBHelper.InputText(deviceID, "11");
+                KAutoHelper.ADBHelper.Tap(deviceID, 721, 713);
+                KAutoHelper.ADBHelper.InputText(deviceID, "1993");
+                KAutoHelper.ADBHelper.Tap(deviceID, 532, 1134);
+
+                // giói tinh
+                KAutoHelper.ADBHelper.Tap(deviceID, 92, 759);
+                KAutoHelper.ADBHelper.Tap(deviceID, 540, 1458);
+
+                // very mail
+                KAutoHelper.ADBHelper.Tap(deviceID, 524, 1497);
+                KAutoHelper.ADBHelper.InputText(deviceID, "bk1e0ggf@linshiyouxiang.net");
+                KAutoHelper.ADBHelper.Tap(deviceID, 532, 775); // tiếp
+                KAutoHelper.ADBHelper.InputText(deviceID, "123456a@");
+                KAutoHelper.ADBHelper.Tap(deviceID, 540, 867); // tiếp
+
+
+                // không tải danh bạ
+                KAutoHelper.ADBHelper.Tap(deviceID, 594, 1276);
+                Sleep(15000);
+
+                // đang nhập 1 lần ấn
+                KAutoHelper.ADBHelper.Tap(deviceID, 779, 1350);
+                KAutoHelper.ADBHelper.Tap(deviceID, 532, 1489);
+
+                // very mail
+                KAutoHelper.ADBHelper.InputText(deviceID, "48449");
+                KAutoHelper.ADBHelper.Tap(deviceID, 555, 755);
+
+                // bỏ qua tất cả
+                KAutoHelper.ADBHelper.Tap(deviceID, 988, 184);
+                KAutoHelper.ADBHelper.Tap(deviceID, 524, 1361);
+                KAutoHelper.ADBHelper.Tap(deviceID, 988, 184);
+                KAutoHelper.ADBHelper.Tap(deviceID, 513, 1292);
+
+                // lấy đường dẫn trang cá nhân
+                KAutoHelper.ADBHelper.Tap(deviceID, 88, 470);
+                KAutoHelper.ADBHelper.Tap(deviceID, 69, 172);
+                KAutoHelper.ADBHelper.Tap(deviceID, 933, 1269);
+                KAutoHelper.ADBHelper.Tap(deviceID, 968, 1392);
+                KAutoHelper.ADBHelper.Tap(deviceID, 521, 1867);
+
+                // ghi file patch
+                KAutoHelper.ADBHelper.Key(deviceID, KAutoHelper.ADBKeyEvent.KEYCODE_APP_SWITCH);
+                KAutoHelper.ADBHelper.Tap(deviceID, 251, 1968);
+                KAutoHelper.ADBHelper.Tap(deviceID, 559, 1133);
+                KAutoHelper.ADBHelper.Tap(deviceID, 525, 1346);
+                KAutoHelper.ADBHelper.Tap(deviceID, 590, 1354);
+                KAutoHelper.ADBHelper.Tap(deviceID, 547, 1512);
+                KAutoHelper.ADBHelper.Key(deviceID, KAutoHelper.ADBKeyEvent.KEYCODE_ENTER);
+                KAutoHelper.ADBHelper.Tap(deviceID, 976, 192);
+
+
+                // clear app
+                KAutoHelper.ADBHelper.Key(deviceID, KAutoHelper.ADBKeyEvent.KEYCODE_APP_SWITCH);
+                KAutoHelper.ADBHelper.Tap(deviceID, 251, 1968);
+                KAutoHelper.ADBHelper.Tap(deviceID, 729, 2037);
+                KAutoHelper.ADBHelper.Tap(deviceID, 528, 1744);
+                KAutoHelper.ADBHelper.Tap(deviceID, 798, 2041);
+
+
+                // về màn hình chính
+                KAutoHelper.ADBHelper.Key(deviceID, KAutoHelper.ADBKeyEvent.KEYCODE_HOME);
+
+                // bất tắt wifi
+                KAutoHelper.ADBHelper.Key(deviceID, KAutoHelper.ADBKeyEvent.KEYCODE_HOME);
+                KAutoHelper.ADBHelper.Swipe(deviceID, 532, 18, 532, 875, 500);
+                KAutoHelper.ADBHelper.Tap(deviceID, 540, 438);
+                Sleep(5000);
+                KAutoHelper.ADBHelper.Tap(deviceID, 540, 438);
+                KAutoHelper.ADBHelper.Key(deviceID, KAutoHelper.ADBKeyEvent.KEYCODE_HOME);
+                // chọn ảnh đại diện
+                //KAutoHelper.ADBHelper.Tap(deviceID, 590, 1875);
+                //KAutoHelper.ADBHelper.Tap(deviceID, 783, 1284);
+                //// cần trượt trong list ảnh
+                //KAutoHelper.ADBHelper.Tap(deviceID, 204, 1585); // con 1 ảnh bất kì
+                //KAutoHelper.ADBHelper.Tap(deviceID, 984, 172);
+                //KAutoHelper.ADBHelper.Tap(deviceID, 992, 176); // lưu ảnh đại diện
+                ////Sleep(5);
+
+                //// bỏ qua tải danh bạ
+                //KAutoHelper.ADBHelper.Tap(deviceID, 972, 184);
+                //KAutoHelper.ADBHelper.Tap(deviceID, 528, 1369);
+                //KAutoHelper.ADBHelper.Tap(deviceID, 984, 180);
+                //KAutoHelper.ADBHelper.Tap(deviceID, 516, 1292);
+
+                //// lưuu tông tin đăng nhập
+                //KAutoHelper.ADBHelper.Tap(deviceID, 810, 2084);
+
+                Task t = new Task(() =>
+                {
+
+
+                });
+                t.Start();
+            }
+        }
+
+        void Sleep(int delay)
+        {
+            while (delay > 0)
+            {
+                Thread.Sleep(1000);
+                delay -= 1000;
+            }
+        }
+    }
+}
